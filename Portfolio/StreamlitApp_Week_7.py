92bf783127d510cd4abc33d48333283a570a67d8
katehiggins169 <katehiggins169@gmail.com>
2026-03-03 17:39:16 -0600

    Reorganize imports and improve session management

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -117,7 +117,7 @@ def display_explanation(input_df, session, aws_bucket):
     preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[:-2])
     input_df_transformed = preprocessing_pipeline.transform(input_df)
     feature_names = full_pipeline[1:4].get_feature_names_out()
-    X_test_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
+    input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
     shap_values = explainer(input_df_transformed)
     
     st.subheader("🔍 Decision Transparency (SHAP)")
@@ -159,6 +159,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
