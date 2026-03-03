c0b144b3d8a1c798b2f8248cce22ed1b4601ac49
katehiggins169 <katehiggins169@gmail.com>
2026-03-03 17:50:35 -0600

    Refactor Streamlit app for improved clarity and structure

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -104,7 +104,7 @@ def call_model_api(input_df):
         raw_pred = predictor.predict(input_df)
         pred_val = pd.DataFrame(raw_pred).values[-1][0]
         mapping = {-1: "SELL", 0: "HOLD", 1: "BUY"}
-        return mapping.get(pred_val, pred_value), 200
+        return mapping.get(pred_val, pred_val), 200
     except Exception as e:
         return f"Error: {str(e)}", 500
 
@@ -159,6 +159,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
