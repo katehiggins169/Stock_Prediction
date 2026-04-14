c875ee55fcd4bf857767ca6459cd0fb3f2a3994f
katehiggins169 <katehiggins169@gmail.com>
2026-04-14 18:04:53 -0500

    Update StreamlitApp_HW6.py

--- Portfolio/StreamlitApp_HW6.py
+++ Portfolio/StreamlitApp_HW6.py
@@ -113,7 +113,7 @@ def call_model_api(input_df):
         # For classification
         raw_pred = predictor.predict(input_df)
         pred_val = pd.DataFrame(raw_pred).values[-1][0]
-        mapping = {-1: "SELL", 0: "HOLD", 1: "BUY"}
+        mapping = {0: "SELL", 1: "HOLD", 2: "BUY"}
         return mapping.get(pred_val), 200
     except Exception as e:
         return f"Error: {str(e)}", 500
